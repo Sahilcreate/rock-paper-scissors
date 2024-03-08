dbb23d7dfa59bbc63e812ee238f83b15bac61522
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2024-03-08 20:57:29 +0530

    Add a pseudocode of the project

---
+++ main.js
@@ -0,0 +1,28 @@
+//get random input from computer from three choice - 'Rock','Paper','Scissor' in a function 'getComputerChoice'
+//get input from user using prompt 
+//make a function that uses two parameters - computer choice and player choice, and plays a game of RockPaperScissors and returns a string that declares winner or tie 
+//repeat the previous function 5 times inside a new function which counts the result of each game and displays the result at the end
+
+//---------------------------------------------
+//ask for confirmation if user want to play the game or not
+//get input from user using prompt
+
+//get random value from computer using following-
+//define 'rock', 'paper', 'scissor' in a array
+//get a random value from the array using mathsfloor(mathsrandom*arraylength) method
+//store it in different variable
+
+//write a function which take two parameter, one of user and one of computer
+//this function determine the winner based on the principles of RockPaperScissors game-
+//Rock beat scissors
+//Paper beat Rock
+//Scissor beak Paper
+//or Tie
+//make two different variables where each of them store either user or computer's number of win
+//after determining the winner, the function return a string that declares the winner of tie of round like: "You Lose! Paper beats Rock"
+
+//make a new function which contain the previous loop inside it and play it 5 times, let's say using loop and at the end declares the winner 
+
+//ask if you want to play again and starts the madness again
+//------------------------------------------------
+
