529dec9219964d86d1e68f15599d88412c31b7e6
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2024-03-12 17:48:47 +0530

    Remove the typeError happening due to 'null' input

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let confirmation = confirm("Would you like to play the game of Rock-Paper-Scissors?");
+let exitConfirmation;
 
 if (confirmation) {
     let playerOption;
@@ -6,60 +7,57 @@ if (confirmation) {
 
     let computerOption;
     let computerSelection;
-    let gameRocking = true;
 
     let playerWinCount = 0;
     let computerWinCount = 0;
-    console.log(playerWinCount); /// 
-    console.log(computerWinCount); ///
+    console.log(playerWinCount); 
+    console.log(computerWinCount); 
 
     for (i=1; i<=5; i++) {
-        playerOption = prompt(`Round ${i}. Please enter your input:`);
-        if (typeof playerOption == "string") {
-            playerOption = playerOption.toLowerCase().trim();
-            if (playerOption === "rock" || playerOption === "paper" || playerOption === "scissor") {
-
+        
+        //loop to offset invalid values such as 'null' which we get when pressed 'Cancel' or any other string
+        for (;;) {
+            playerOption = prompt(`Round ${i}. Please enter your input:`)
+            if (typeof(playerOption) == 'string') {
+                playerOption = playerOption.toLowerCase().trim();
+                if (playerOption === "rock" || playerOption === "paper" || playerOption === "scissor") {
+                    playerSelection = playerOption;
+                    break;
+                } else {
+                    alert("You have given an invalid input. Please write from 'rock', 'paper', and 'scissor'.");
+                    continue;
+                }
             } else {
-                alert(`You have given an invalid response. Please only write from 'Rock', 'Paper' and 'Scissor' otherwise the game will be closed as I don't know any other method to fix this typeError. Skill Issue ig :)`);
-                playerOption = prompt(`Round ${i}. Please enter your input:`);
-            }
-        } else {
-            alert(`You have given an invalid response. Please only write from 'Rock', 'Paper' and 'Scissor' otherwise the game will be closed as I don't know any other method to fix this typeError. Skill Issue ig :)`);
-            playerOption = prompt(`Round ${i}. Please enter your input:`);
+                exitConfirmation = confirm("You have given an invalid input. Would you like to exit the game?");
+
+                if (exitConfirmation) {
+                    break;
+                } else {
+                    continue;
+                }
+            } 
         }
 
-        if(typeof playerOption == "string") {
-            playerOption = playerOption.toLowerCase().trim();
-            if (playerOption === "rock" || playerOption === "paper" || playerOption === "scissor") {
-                playerSelection = playerOption;
-            } else {
-                gameRocking = false;
-                break;
-            }
-        } else {
-            gameRocking = false;
+        if (exitConfirmation) {
             break;
         }
 
-        console.log(playerSelection); ///
+        console.log(playerSelection); 
 
         computerOption = ["rock","paper","scissor"];
         computerSelection = computerOption[(Math.floor(Math.random() * computerOption.length))];
 
         function playRound(playerSelection, computerSelection) {
             if ((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissor" && computerSelection === "paper")) {
-                alert (`You Win! ${playerSelection} beats ${computerSelection}.`);
-                playerWinCount++;
-                alert(`Score: ${playerWinCount} - ${computerWinCount}`);
+                alert (`You Win! ${playerSelection} beats ${computerSelection}. \n Score: ${++playerWinCount} - ${computerWinCount}`);
+            
             } else if (playerSelection === computerSelection) {
-                alert (`It's a Tie! ${playerSelection} doesn't beat ${computerSelection}.`);
-                playerWinCount++;
-                computerWinCount++;
-                alert(`Score: ${playerWinCount} - ${computerWinCount}`);
+                alert (`It's a Tie! ${playerSelection} doesn't beat ${computerSelection}. \n Score: ${++playerWinCount} - ${++computerWinCount}`);
+               
             } else {
-                alert(`You Lose! ${computerSelection} beats ${playerSelection}.`);
-                computerWinCount++;
-                alert(`Score: ${playerWinCount} - ${computerWinCount}`);
+                alert(`You Lose! ${computerSelection} beats ${playerSelection}.
+                \n Score: ${playerWinCount} - ${++computerWinCount}`);
+                
             }
         }
 
@@ -67,14 +65,13 @@ if (confirmation) {
         console.log(playerWinCount); /// 
         console.log(computerWinCount); ///
     }
-    if (gameRocking) {
-        if (playerWinCount>computerWinCount) {
+        
+    if (playerWinCount>computerWinCount) {
             alert(`Congratulations! You won by ${playerWinCount} - ${computerWinCount}.`);
-        } else if (playerWinCount === computerWinCount) {
+    } else if (playerWinCount === computerWinCount) {
             alert(`It's a Tie by ${playerWinCount} - ${computerWinCount}.`);
-        } else {
+    } else {
             alert(`You Lose! Score is ${playerWinCount} - ${computerWinCount}. Better luck next time.`);
-        }
     }
 
 } else {
